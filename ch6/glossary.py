5153fe6e4e8e5fd21f4ea09f85b90ca63f37b5ea
fjgl <fjluartes@gmail.com>
2018-08-18 14:33:10 +0800

    ch6: added glossary.py

---
+++ ch6/glossary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# glossary.py: Exercise 6-3
+# 18 Aug 2018 | fjgl
+# 6-3. Glossary
+# print, list, for, if, dictionary
+glossary = {
+    'print': 'Python keyword used to display data. (usually strings)',
+    'list': 'A collection of items in a particular order.',
+    'for': 'Python keyword used to loop into a list, tuple, or dictionary.',
+    'if': 'Python keyword for conditional statements.',
+    'dictionary': 'A collection of key-value pairs.'
+    }
+print("print: " + glossary['print'] + "\n")
+print("list: " + glossary['list'] + "\n")
+print("for: " + glossary['for'] + "\n")
+print("if: " + glossary['if'] + "\n")
+print("dictionary: " + glossary['dictionary'] + "\n")
