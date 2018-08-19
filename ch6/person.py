0c5960ee892cfe66d8c4cffbf2d77afb3483eb36
fjgl <fjluartes@gmail.com>
2018-08-19 12:28:47 +0800

    ch6: added people to person.py

--- ch6/person.py
+++ ch6/person.py
@@ -1,15 +1,48 @@
 #!/usr/bin/env python
-# person.py: Exercise 6-1
+# person.py: Exercise 6-1, 6-7
 # 18 Aug 2018 | fjgl
 # 6-1. Person
-person = {
-    'first_name': 'maan',
-    'last_name': 'bajan',
-    'age': 23,
-    'city': 'muntinlupa'
+#person = {
+#    'first': 'alan',
+#    'last': 'turing',
+#    'age': 41,
+#    'city': 'london'
+#    }
+#print("Name: " + person['first'].title() + " " +
+#      person['last'].title() +
+#      "\nAge: " + str(person['age']) +
+#      "\nCity: " + person['city'].title())
+
+# 6-7. People
+people = {
+    'aturing': {
+        'first': 'alan',
+        'last': 'turing',
+        'age': 41,
+        'city': 'manchester'
+        },
+    'dknuth': {
+        'first': 'donald',
+        'last': 'knuth',
+        'age': 80,
+        'city': 'stanford'
+        },
+    'sjones': {
+        'first': 'simon',
+        'last': 'jones',
+        'age': 60,
+        'city': 'cambridge'
+        },
+    'jarmstrong': {
+        'first': 'joe',
+        'last': 'armstrong',
+        'age': 67,
+        'city': 'stockholm'
+        }
     }
 
-print("Name: " + person['first_name'].title() + " " +
-      person['last_name'].title() +
-      "\nAge: " + str(person['age']) +
-      "\nCity: " + person['city'].title())
+for person in people.values():
+    full_name = person['first'] + " " + person['last']
+    print("\nName: " + full_name.title())
+    print("\tAge: " + str(person['age']))
+    print("\tCity: " + person['city'].title())
