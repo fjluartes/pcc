860f28c899ddd1154a1408dcb4d73af2c73b2198
fjgl <fjluartes@gmail.com>
2018-08-21 10:03:25 +0800

    ch7: added exercise 7-10

--- ch7/dream_vacation.py
+++ ch7/dream_vacation.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 # dream_vacation.py: Exercise 7-10
 # 21 Aug 2018 | fjgl
+places = {}
+active = True
+
+while active:
+    name = input("\nWhat is your name? ")
+    place = input("If you could visit one place in the world, where would you go? ")
+    places[name] = place
+
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat.lower() == 'no':
+        active = False
+
+print("\n--- Poll Results ---")
+for name, place in places.items():
+    print(name.title() + " would like to go to " + place.title() + ".")
