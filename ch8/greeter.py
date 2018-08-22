7ff633d7be7366fbc11a37abf779fd62b8b6cba2
fjgl <fjluartes@gmail.com>
2018-08-22 17:41:45 +0800

    ch8: edited greeter.py, added person.py

--- ch8/greeter.py
+++ ch8/greeter.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
-# greeter.py: defining a function
+# greeter.py: defining a function, function with while loop
 # 22 Aug 2018 | fjgl
-def greet_user(username):
-    """Display a simple greeting."""
-    print("Hello, " + username.title() + "!")
-
-greet_user('jesse')
-
+#def greet_user(username):
+#    """Display a simple greeting."""
+#    print("Hello, " + username.title() + "!")
+#greet_user('jesse')
 # parameter: username
 # argument: 'jesse'
+
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print("\nHello, " + formatted_name + "!")
---
+++ ch8/person.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# person.py: returning a dictionary
+# 22 Aug 2018 | fjgl
+def build_person(first_name, last_name, age=''):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
