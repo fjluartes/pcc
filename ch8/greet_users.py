ac3dc107b253aa0ceeb49dee6ae765eff7652b97
fjgl <fjluartes@gmail.com>
2018-08-22 19:30:21 +0800

    ch8: passing and modifying lists in a function

---
+++ ch8/greet_users.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# greet_users.py: passing a list
+# 22 Aug 2018 | fjgl
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
