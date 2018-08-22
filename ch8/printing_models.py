782905d8743e9e930d292de3f0345f262caf8b1b
fjgl <fjluartes@gmail.com>
2018-08-22 19:36:10 +0800

    ch8: prevent a function from modifying a list

--- ch8/printing_models.py
+++ ch8/printing_models.py
@@ -11,6 +11,10 @@ def print_models(unprinted_designs, completed_models):
         print("Printing model: " + current_design)
         completed_models.append(current_design)
 
+# pass a copy of the list in the function (keep original list)
+# function_name(list_name[:])
+# def print_models(unprinted_designs[:], completed_models):
+
 def show_completed_models(completed_models):
     """Display all completed models."""
     print("\nThe following models have been printed:")
@@ -22,3 +26,4 @@ completed_models = []
 
 print_models(unprinted_designs, completed_models)
 show_completed_models(completed_models)
+
