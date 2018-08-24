ab0218d550f0506dd6e47665c30dcea83210330a
fjgl <fjluartes@gmail.com>
2018-08-24 18:36:46 +0800

    ch8: edited exercise 8-16

--- ch8/make_cars.py
+++ ch8/make_cars.py
@@ -2,8 +2,11 @@
 # make_cars.py: Exercise 8-16
 # 24 Aug 2018 | fjgl
 #import cars
-from cars import make_car
+#from cars import make_car
+#from cars import make_car as mc
+#import cars as c
+from cars import *
 
-car = .make_car('subaru', 'outback', color='blue', tow_package=True)
+car = make_car('subaru', 'outback', color='blue', tow_package=True)
 print(car)
 
