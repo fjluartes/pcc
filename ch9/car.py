fe746fb39938da366ea0db9d6854ea4945066995
fjgl <fjluartes@gmail.com>
2018-08-25 20:59:30 +0800

    ch9: added car.py

--- ch9/car.py
+++ ch9/car.py
@@ -8,11 +8,20 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Return a neatly formatted descriptive name"""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print("This car has " + str(self.odometer_reading) + " miles in it.")
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+
+# modifying an attribute's value directly
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
