e28693c15a3337a1bc528aa360388070c26c6cb9
fjgl <fjluartes@gmail.com>
2018-08-26 19:43:28 +0800

    ch9: edited car.py:

--- ch9/car.py
+++ ch9/car.py
@@ -19,9 +19,22 @@ class Car():
         """Print a statement showing the car's mileage."""
         print("This car has " + str(self.odometer_reading) + " miles in it.")
 
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
+    def update_odometer(self, mileage):
+        """Set odometer reading to the given value."""
+        self.odometer_reading = mileage
 
-# modifying an attribute's value directly
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+#my_new_car = Car('audi', 'a4', 2016)
+#print(my_new_car.get_descriptive_name())
+#my_new_car.update_odometer(23)
+#my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
