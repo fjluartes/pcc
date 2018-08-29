d1f662b48b64b0f5e6b2cee4695bd5488998ca91
fjgl <fjluartes@gmail.com>
2018-08-29 17:21:16 +0800

    ch10: writing and appending into a file

---
+++ ch10/write_message.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# write_message.py: writing to an empty file
+# 29 Aug 2018 | fjgl
+filename = 'programming.txt'
+
+#with open(filename, 'w') as file_object:
+#    file_object.write("I love programming.\n")
+#    file_object.write("I love creating new games.\n")
+
+# appending to a file
+with open(filename, 'a') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
