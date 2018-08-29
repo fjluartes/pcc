1ef191eea3602642c9e78a2401c57fa2c6172ba2
fjgl <fjluartes@gmail.com>
2018-08-29 16:18:45 +0800

    ch10: pi million digits

--- ch10/pi_string.py
+++ ch10/pi_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # pi_string.py: working with file's contents
 # 29 Aug 2018 | fjgl
-filename = 'pi_digits.txt'
+#filename = 'pi_digits.txt'
+filename = 'pi_million_digits.txt'
 
 with open(filename) as file_object:
     lines = file_object.readlines()
@@ -10,5 +11,13 @@ pi_string = ''
 for line in lines:
     pi_string += line.strip()
 
-print(pi_string)
-print(len(pi_string))
+# pi_million_digits
+#print(pi_string[:52] + "...")
+#print(len(pi_string))
+
+# birthday in pi
+birthday = input("Enter your birthday in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday des not appear in the first million digits of pi!")
