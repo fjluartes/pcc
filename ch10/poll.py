410ac68bec3b824568a1755ee62ca7dee3be749d
fjgl <fjluartes@gmail.com>
2018-08-29 19:39:33 +0800

    ch10: exercise 10-5

--- ch10/poll.py
+++ ch10/poll.py
@@ -6,15 +6,14 @@ poll_active = True
 responses = {}
 with open(filename, 'a') as file_object:
     while poll_active:
-        print("\nEnter 'q' to quit poll")
         name = input("What is your name? ")
-        if name == 'q':
+        reason = input("Why do you like programming? ")
+        responses[name] = reason
+        stop = input("\nWould you let another person respond? (yes/no) ")
+        if stop.lower() == 'no':
             poll_active = False
-        else:
-            reason = input("Why do you like programming? ")
-            responses[name] = reason
 
-    print("--- Poll Results ---")
-    for name, response in responses.items():
+    print("\n--- Poll Results ---")
+    for name, reason in responses.items():
         file_object.write(name.title() + ": " + reason + ".\n")
-        print(name.title() + ": " + reason + "."
+        print(name.title() + ": " + reason + ".")
