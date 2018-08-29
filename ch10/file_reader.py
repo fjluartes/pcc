7545ccf621d2b9ef12daeaaba0be9b1cc23f4b3b
fjgl <fjluartes@gmail.com>
2018-08-29 15:08:32 +0800

    ch10: file_reader.py, pi_string.py

--- ch10/file_reader.py
+++ ch10/file_reader.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 # file_reader.py: reading an entire file
 # 29 Aug 2018 | fjgl
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents)
+filename = 'pi_digits.txt'
+
+# reading an entire file
+# rstrip(): remove whitespace
+#with open(filename) as file_object:
+#    contents = file_object.read()
+#    print(contents.rstrip())
+
+# reading line by line
+#with open(filename) as file_object:
+#    for line in file_object:
+#        print(line.rstrip())
+
+# making list of lines from a file
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
---
+++ ch10/pi_string.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# pi_string.py: working with file's contents
+# 29 Aug 2018 | fjgl
