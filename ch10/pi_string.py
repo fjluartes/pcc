02bc3ffe1789eec81b89c6cbe00a4f13f6a9b2d2
fjgl <fjluartes@gmail.com>
2018-08-29 15:50:27 +0800

    ch10: working with file contents

--- ch10/pi_string.py
+++ ch10/pi_string.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 # pi_string.py: working with file's contents
 # 29 Aug 2018 | fjgl
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
