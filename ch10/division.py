3a1820f52516219427f34b50fd672318f15cdb8d
fjgl <fjluartes@gmail.com>
2018-08-30 17:37:30 +0800

    ch10: try-except-else

--- ch10/division.py
+++ ch10/division.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 # division.py: exceptions
 # 30 Aug 2018 | fjgl
-print(5/0)
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    # try-except-else block
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    else:
+        print(answer)
