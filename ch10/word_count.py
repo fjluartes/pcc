32a9f03b60067c94398b872f51a6109904f42d5e
fjgl <fjluartes@gmail.com>
2018-08-30 18:43:28 +0800

    ch10: failing silently

--- ch10/word_count.py
+++ ch10/word_count.py
@@ -7,8 +7,10 @@ def count_words(filename):
         with open(filename) as f_obj:
             contents = f_obj.read()
     except FileNotFoundError:
-        msg = "Sorry, the file " + filename + " does not exist."
-        print(msg)
+        #msg = "Sorry, the file " + filename + " does not exist."
+        #print(msg)
+        # failing silently
+        pass
     else:
         # Count the approximate number of words in the file
         words = contents.split()
@@ -16,5 +18,6 @@ def count_words(filename):
         print("The file " + filename + " has about " + str(num_words) +
               " words.")
 
-filename = 'alice.txt'
-count_words(filename)
+filenames = ['alice.txt', 'siddhartha1.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    count_words(filename)
