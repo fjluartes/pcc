5da43c305fce2ac71f635be8a4fc042b3133005d
fjgl <fjluartes@gmail.com>
2018-09-02 06:49:32 +0800

    ch10: Exercise 10-12

--- ch10/favorite_numbers.py
+++ ch10/favorite_numbers.py
@@ -3,7 +3,32 @@
 # 1 Sep 2018 | fjgl
 import json
 
-filename = 'favorite_numbers.json'
-fave_number = input("What is your favorite number? ")
-with open(filename, 'w') as f_obj:
-    json.dump(fave_number, f_obj)
+def get_stored_number():
+    """Display stored favorite number of user if available."""
+    filename = 'favorite_numbers.json'
+    try:
+        with open(filename) as f_obj:
+            fave_number = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return fave_number
+
+def get_new_number():
+    """Get new favorite number from user."""
+    filename = 'favorite_numbers.json'
+    fave_number = input("What is your favorite number? ")
+    with open(filename, 'w') as f_obj:
+        json.dump(fave_number, f_obj)
+    return fave_number
+
+def show_number():
+    """Show favorite number of user."""
+    fave_number = get_stored_number()
+    if fave_number:
+        print("I know your favorite number! It's " + fave_number + "!")
+    else:
+        fave_number = get_new_number()
+        print("I'll remember your favorite number!")
+
+show_number()
--- ch10/read_numbers.py
+++ ch10/read_numbers.py
@@ -3,8 +3,11 @@
 # 1 Sep 2018 | fjgl
 import json
 
-filename = 'favorite_numbers.json'
-with open(filename) as f_obj:
-    fave_number = json.load(f_obj)
+def get_stored_number():
+    """Show favorite number of user."""
+    filename = 'favorite_numbers.json'
+    with open(filename) as f_obj:
+        fave_number = json.load(f_obj)
+    print("I know your favorite number! It's " + fave_number + "!")
 
-print("I know your favorite number! It's " + fave_number + "!")
+get_stored_number()
