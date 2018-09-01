8ceae69af99b2c6a0f4d8f093ff08996e4f55c6c
fjgl <fjluartes@gmail.com>
2018-09-01 13:42:48 +0800

    ch10: refactoring

--- ch10/remember_me.py
+++ ch10/remember_me.py
@@ -3,9 +3,21 @@
 # 1 Sep 2018 | fjgl
 import json
 
-username = input("What is your name? ")
+# Refactoring
+def greet_user():
+    """Greet the user by name."""
+    # Load the username, if it has been stored previously.
+    #   Otherwise, prompt for the username and store it.
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        username = input("What is your name? ")
+        with open(filename, 'w') as f_obj:
+            json.dump(username, f_obj)
+            print("We'll remember you when you come back, " + username + "!")
+    else:
+        print("Welcome back, " + username + "!")
 
-filename = 'username.json'
-with open(filename, 'w') as f_obj:
-    json.dump(username, f_obj)
-    print("We'll remember you when you come back, " + username + "!")
+greet_user()
