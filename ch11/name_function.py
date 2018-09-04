4e88edd1ab6476059a4df7f7694abd31a48adc4f
fjgl <fjluartes@gmail.com>
2018-09-04 08:56:43 +0800

    ch11: testing a function

---
+++ ch11/name_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# name_function.py: testing a function
+# 4 Sep 2018 | fjgl
+def get_formatted_name(first, last):
+    """Generate a neatly formatted full name."""
+    full_name = first + ' ' + last
+    return full_name.title()
