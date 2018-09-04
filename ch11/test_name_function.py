e652c230794a7c76ef51512e368f8acbf6019d01
fjgl <fjluartes@gmail.com>
2018-09-04 09:21:31 +0800

    ch11: adding tests

--- ch11/test_name_function.py
+++ ch11/test_name_function.py
@@ -11,4 +11,9 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
 
+    def test_first_middle_last(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
 unittest.main()
