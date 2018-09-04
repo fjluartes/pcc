a1bfffcb09c24efdd61f83fe8d916834b320bccf
fjgl <fjluartes@gmail.com>
2018-09-04 09:07:00 +0800

    ch11: test_name_function.py

---
+++ ch11/test_name_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# test_name_function.py: unit test for name_function.py
+# 4 Sep 2018 | fjgl
+import unittest
+from name_function import get_formatted_name
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+unittest.main()
