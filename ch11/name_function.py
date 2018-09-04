55fde129bc66f5ff1093a04e74322e528b191b80
fjgl <fjluartes@gmail.com>
2018-09-04 09:14:41 +0800

    ch11: edited name_function.py

--- ch11/name_function.py
+++ ch11/name_function.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # name_function.py: testing a function
 # 4 Sep 2018 | fjgl
-def get_formatted_name(first, last):
+def get_formatted_name(first, last, middle=''):
     """Generate a neatly formatted full name."""
-    full_name = first + ' ' + last
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+    else:
+        full_name = first + ' ' + last
     return full_name.title()
