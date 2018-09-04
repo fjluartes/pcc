166741250a015d79e460718bf21fed79ad26e44d
fjgl <fjluartes@gmail.com>
2018-09-04 10:16:42 +0800

    ch11: exercise 11-1 (test_cities)

--- ch11/test_cities.py
+++ ch11/test_cities.py
@@ -7,8 +7,8 @@ from city_functions import get_formatted_city
 class CitiesTestCase(unittest.TestCase):
     """Test cases for 'city_functions.py'."""
     def test_city_country(self):
-        """Do cities like 'Paris, France' work?"""
-        formatted_city = get_formatted_city('paris', 'france')
-        self.assertEqual(formatted_city, 'Paris, France')
+        """Do cities like 'Santiago, Chile' work?"""
+        formatted_city = get_formatted_city('santiago', 'chile')
+        self.assertEqual(formatted_city, 'Santiago, Chile')
 
 unittest.main()
