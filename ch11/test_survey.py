7bebf1d4687616c5d84baab3a2d320383d313945
fjgl <fjluartes@gmail.com>
2018-09-05 17:36:27 +0800

    ch11: test survey.py

---
+++ ch11/test_survey.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# test_survey.py: test cases for AnonymousSurvey
+# 5 Sep 2018 | fjgl
+import unittest
+from survey import AnonymousSurvey
+
+class TestAnonymousSurvey(unittest.TestCase):
+    """Tests for the class AnonymousSurvey."""
+    def test_store_single_response(self):
+        """Test that a sigle response is stored properly."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        my_survey.store_response('English')
+
+        self.assertIn('English', my_survey.responses)
+
+unittest.main()
